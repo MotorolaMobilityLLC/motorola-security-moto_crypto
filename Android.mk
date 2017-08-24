0a2b791e7050864f3b1783731bfc893360dc6a43
Kurinji <a20760@motorola.com>
2017-08-24 23:56:24 +0530

    Change the path from /system/lib/modules to /vendor/lib/modules
    Change-Id: I91892197c62cae7311f3c76671d183cc2c4c873b
    Reviewed-on: https://gerrit.mot.com/1047740
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Srinivas Lakshmi Panchumarthi <plsrinivas@motorola.com>
    Submit-Approved: Jira Key

--- Android.mk
+++ Android.mk
@@ -7,15 +7,15 @@ KBUILD_OPTIONS            := TARGET_BUILD_VARIANT=$(TARGET_BUILD_VARIANT)
 LOCAL_MODULE              := moto_crypto.ko
 LOCAL_MODULE_TAGS         := optional
 LOCAL_MODULE_DEBUG_ENABLE := false
-LOCAL_MODULE_PATH         := $(TARGET_OUT)/lib/modules
+LOCAL_MODULE_PATH         := $(TARGET_OUT_VENDOR)/lib/modules
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
 include $(CLEAR_VARS)
 
-${TARGET_OUT}/lib/modules/moto_crypto_hmac_sha256: $(TARGET_OUT)/lib/modules/moto_crypto.ko
-	motorola/security/moto_crypto/scripts/fips_module_hmac.py 3c091d83745f3ed32cab47458950bca648561bc54d738fe5ee34235ff1100d4a $< > ${TARGET_OUT}/lib/modules/moto_crypto_hmac_sha256
+${TARGET_OUT_VENDOR}/lib/modules/moto_crypto_hmac_sha256: $(TARGET_OUT_VENDOR)/lib/modules/moto_crypto.ko
+	motorola/security/moto_crypto/scripts/fips_module_hmac.py 3c091d83745f3ed32cab47458950bca648561bc54d738fe5ee34235ff1100d4a $< > ${TARGET_OUT_VENDOR}/lib/modules/moto_crypto_hmac_sha256
 
 LOCAL_MODULE                  := generate_moto_crypto_signature
 LOCAL_MODULE_TAGS             := optional
-LOCAL_ADDITIONAL_DEPENDENCIES := ${TARGET_OUT}/lib/modules/moto_crypto_hmac_sha256
+LOCAL_ADDITIONAL_DEPENDENCIES := ${TARGET_OUT_VENDOR}/lib/modules/moto_crypto_hmac_sha256
 include $(BUILD_PHONY_PACKAGE)
